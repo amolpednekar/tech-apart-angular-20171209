224b89d75601af9583855092aa6f24688b7855b0
amolpednekar <amol0202@gmail.com>
2017-12-09 13:35:11 +0530

    Session 1 - Added basic html, scss, basic binding

--- fly-with-angular/src/app/app.component.ts
+++ fly-with-angular/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  trip_name = 'Trip to Mars';
+  trip_price = 20000;
+  trip_duration = '3 Earth Years'
+  trip_description = 'Race through the Solar System to reach the Red planet.'
 }
